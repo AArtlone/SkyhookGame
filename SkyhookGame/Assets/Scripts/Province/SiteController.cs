0a03243076289765ff7db45089c0bb7444c83fd3
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-10-23 17:17:05 +0200

    Added basic province template

---
+++ SkyhookGame/Assets/Scripts/Province/SiteController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SiteController : MonoBehaviour
+{
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireSphere(transform.position, 0.6f);
+	}
+}
