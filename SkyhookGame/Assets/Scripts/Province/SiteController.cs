137373c634327e433fcb2d0848706b8dd38a145d
Николай Иванов <niki.ivanov98@gmail.com>
2020-10-23 19:04:16 +0200

    Added basic template and site types (#8)

--- SkyhookGame/Assets/Scripts/Province/SiteController.cs
+++ SkyhookGame/Assets/Scripts/Province/SiteController.cs
@@ -2,9 +2,25 @@
 
 public class SiteController : MonoBehaviour
 {
+	public SiteType siteType;
+
 	private void OnDrawGizmos()
 	{
-		Gizmos.color = Color.cyan;
+		switch (siteType)
+		{
+			case SiteType.Circumsolar:
+				Gizmos.color = Color.cyan;
+				break;
+			case SiteType.Satellite:
+				Gizmos.color = Color.magenta;
+				break;
+			case SiteType.Surface:
+				Gizmos.color = Color.blue;
+				break;
+			default:
+				break;
+		}
+
 		Gizmos.DrawWireSphere(transform.position, 0.6f);
 	}
 }
---
+++ SkyhookGame/Assets/Scripts/Settlement/SettlementView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SettlementView : MonoBehaviour
+{
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		for (int i = 0; i < 3; i++)
+		{
+			Gizmos.DrawWireSphere(transform.position, (float)((i + 1.3 + 3) * 1.5));
+		}
+	}
+}
