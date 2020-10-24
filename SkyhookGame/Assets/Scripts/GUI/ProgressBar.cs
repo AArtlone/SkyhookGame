41b8f4226ba3824caeed7561f87b9f9c55f90c9d
Artem <artem.stolyga@gmail.com>
2020-10-24 18:06:59 +0200

    Created a progress bar prefab

---
+++ SkyhookGame/Assets/Scripts/GUI/ProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField] private Image bar = default;
+
+    private void Awake()
+    {
+        ResetBar(true);
+    }
+
+    public void SetFillAmount(float value)
+    {
+        bar.fillAmount = value;
+    }
+
+    private void ResetBar(bool full)
+    {
+        if (full)
+            bar.fillAmount = 1f;
+        else
+            bar.fillAmount = 0f;
+    }
+}
