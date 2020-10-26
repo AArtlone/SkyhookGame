2e304d815a4fe14e408881bb65e9f171c7119568
Artem <artem.stolyga@gmail.com>
2020-10-26 20:33:02 +0100

    Setting up greybox UI for Manufactory WIP

---
+++ SkyhookGame/Assets/Scripts/Institutions/Manufactory/ManufactoryGridCell.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ManufactoryGridCell : CellData<ManufactoryGridCellData>
+{
+
+}
+
+public class CellData<T> : MonoBehaviour where T : CellData
+{
+
+}
+
+public class ManufactoryGridCellData : CellData
+{
+
+}
+
+public class CellData
+{
+    public bool IsSelected;
+}
