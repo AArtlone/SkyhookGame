ba749d1d14a4e840b3dbde8dcbbb8440f03a43df
Artem <artem.stolyga@gmail.com>
2020-10-26 12:52:39 +0100

    Cosmic Port WIP Greybox UI for Docks, docks availability based on the CosmicPort level

---
+++ SkyhookGame/Assets/Scripts/Institutions/CosmicPort/Dock.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dock : MonoBehaviour
+{
+    [SerializeField] private MyButton button = default;
+    [SerializeField] private TextMeshProUGUI statusText = default;
+    
+    private Image backgroundImage = default;
+
+    private void Awake()
+    {
+        backgroundImage = GetComponent<Image>();
+
+        if (backgroundImage == null)
+        {
+            Debug.LogError("Image component does not exist on " + gameObject.name);
+            enabled = false;
+            return;
+        }
+    }
+
+    public void SetAvailable()
+    {
+        UpdateVisualToAvailable();
+
+        button.SetInteractable(true);
+
+        statusText.text = "Available";
+    }
+
+    private void UpdateVisualToAvailable()
+    {
+        var color = backgroundImage.color;
+
+        backgroundImage.color = new Color(color.r, color.g, color.b, 1);
+    }
+}
