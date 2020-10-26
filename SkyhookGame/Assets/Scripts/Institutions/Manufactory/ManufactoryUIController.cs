20be687cd45cc6cb2721b73f91c5593ed7e82c32
Artem <artem.stolyga@gmail.com>
2020-10-26 20:33:02 +0100

    Setting up greybox UI for Manufactory WIP

---
+++ SkyhookGame/Assets/Scripts/Institutions/Manufactory/ManufactoryUIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ManufactoryUIController : MonoBehaviour
+{
+    [Space(10f)]
+    [SerializeField] private GameObject preview = default;
+    [SerializeField] private GameObject upgradeView = default;
+    [SerializeField] private GameObject manufactoryView = default;
+    [SerializeField] private GameObject buildShipView = default;
+
+    private void Awake()
+    {
+        preview.SetActive(false);
+        upgradeView.SetActive(false);
+        manufactoryView.gameObject.SetActive(false);
+    }
+
+    public void ShowBuildShipView()
+    {
+        buildShipView.SetActive(true);
+
+        //Pass ship information to the view
+    }
+}
