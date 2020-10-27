0071a6586ae14a3978f1251d7a59fcf3379fda68
Artem <artem.stolyga@gmail.com>
2020-10-27 14:07:48 +0100

    Handling cell selection in SelectableController

--- SkyhookGame/Assets/Scripts/GUI/SelectableCell.cs
+++ SkyhookGame/Assets/Scripts/GUI/SelectableCell.cs
@@ -1,18 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
+[RequireComponent(typeof(MyButton))]
 public abstract class SelectableCell<T> : MonoBehaviour where T : SelectableCellData
 {
+    public Action<SelectableCell<T>> onCellPress;
+
     public T data;
+
+    [HideInInspector] public MyButton myButton;
 
     public void SetData(T data)
     {
         this.data = data;
+
+        myButton = GetComponent<MyButton>();
+        
+        myButton.SetInteractable(true);
+
+        myButton.onClick += MyButton_OnClick;
+    }
+
+    private void OnDestroy()
+    {
+        myButton.onClick -= MyButton_OnClick;
     }
 
     public abstract void Refresh();
+
+    public virtual void MyButton_OnClick()
+    {
+        onCellPress?.Invoke(this);
+    }
 }
 
 public class SelectableCellData
 {
-    public bool IsSelected;
+
 }
