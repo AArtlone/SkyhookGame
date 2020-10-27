ba9312011b60574428af3a2fde89413178db0924
Artem <artem.stolyga@gmail.com>
2020-10-27 11:02:33 +0100

    Setting up UI for Manufactory and Storage view, set up build ship view

---
+++ SkyhookGame/Assets/Scripts/Institutions/Manufactory/BuildShipView.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class BuildShipView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameText = default;
+    [SerializeField] private TextMeshProUGUI priceText = default;
+    [SerializeField] private TextMeshProUGUI reqResourcesText = default;
+
+    public void ShowView(ShipRecipe shipRecipe)
+    {
+        nameText.text = shipRecipe.shipName;
+        priceText.text = shipRecipe.price.ToString();
+        reqResourcesText.text = shipRecipe.reqResources.ToString();
+
+        gameObject.SetActive(true);
+    }
+}
