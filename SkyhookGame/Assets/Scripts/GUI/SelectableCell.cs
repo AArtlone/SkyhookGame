6bb3638464034855eb0bd1a1960a70e1eacfa8f2
Artem <artem.stolyga@gmail.com>
2020-10-27 11:00:21 +0100

    Created a generic setup for selectable views and its' cells

---
+++ SkyhookGame/Assets/Scripts/GUI/SelectableCell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class SelectableCell<T> : MonoBehaviour where T : SelectableCellData
+{
+    public T data;
+
+    public void SetData(T data)
+    {
+        this.data = data;
+    }
+
+    public abstract void Refresh();
+}
+
+public class SelectableCellData
+{
+    public bool IsSelected;
+}
