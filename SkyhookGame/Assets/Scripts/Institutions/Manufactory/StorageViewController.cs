0ac50d70629e46d55b10081c6a44f4f2e64820ba
Artem <artem.stolyga@gmail.com>
2020-10-27 11:01:31 +0100

    Created selectable views and its' cells for Manufactory and Storage views

---
+++ SkyhookGame/Assets/Scripts/Institutions/Manufactory/StorageViewController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class StorageViewController : SelectableController<StorageGridCell, StorageGridCellData>
+{
+    [SerializeField] private GameObject emptyStorageText = default;
+
+    private List<StorageGridCell> shipsInStorage = new List<StorageGridCell>();
+
+    private void Awake()
+    {
+        if (shipsInStorage.Count == 0)
+        {
+            emptyStorageText.SetActive(true);
+            return;
+        }
+
+        List<StorageGridCellData> dataSet = new List<StorageGridCellData>(shipsInStorage.Count);
+
+        shipsInStorage.ForEach(e => dataSet.Add(new StorageGridCellData(e.data.shipName)));
+
+        SetDataSet(dataSet);
+
+        Initialize();
+    }
+}
