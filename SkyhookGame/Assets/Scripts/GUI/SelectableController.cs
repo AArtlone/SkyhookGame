c1af96243b0e4a95fa0b519c2c307f689f9e75ad
Artem <artem.stolyga@gmail.com>
2020-10-27 14:07:48 +0100

    Handling cell selection in SelectableController

--- SkyhookGame/Assets/Scripts/GUI/SelectableController.cs
+++ SkyhookGame/Assets/Scripts/GUI/SelectableController.cs
@@ -8,7 +8,11 @@ public class SelectableController<T1, T2> : MonoBehaviour
     [SerializeField] private T1 cellPrefab = default;
     [SerializeField] private RectTransform contentContainer = default;
 
+    private List<T1> cells;
+
     private List<T2> dataSet;
+
+    private int selectedCell;
 
     protected virtual void SetDataSet(List<T2> dataSet)
     {
@@ -17,6 +21,9 @@ public class SelectableController<T1, T2> : MonoBehaviour
 
     protected virtual void Initialize()
     {
+        if (cells == null)
+            cells = new List<T1>(dataSet.Count);
+
         foreach (T2 data in dataSet)
         {
             var cell = Instantiate(cellPrefab, contentContainer);
@@ -24,6 +31,31 @@ public class SelectableController<T1, T2> : MonoBehaviour
             cell.SetData(data);
 
             cell.Refresh();
+
+            cell.onCellPress += Cell_OnCellPress;
+
+            cells.Add(cell);
         }
+    }
+
+    protected void RefreshView()
+    {
+        cells.ForEach(c => Destroy(c));
+
+        cells = null;
+
+        Initialize();
+    }
+
+    protected virtual void Cell_OnCellPress(SelectableCell<T2> cell)
+    {
+        int selectedCellIndex = cells.IndexOf(cell as T1);
+
+        selectedCell = selectedCellIndex;
+    }
+
+    public T1 GetSelectedCell()
+    {
+        return cells[selectedCell];
     }
 }
