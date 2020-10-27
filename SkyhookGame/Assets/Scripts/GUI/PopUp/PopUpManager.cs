415313e0e3ace11aaf5741205d6ad4c365c8926d
Artem <artem.stolyga@gmail.com>
2020-10-27 20:04:12 +0100

    Created a PopUpManager, its functionality and a PopUpPrefab

---
+++ SkyhookGame/Assets/Scripts/GUI/PopUp/PopUpManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public static class PopUpManager
+{
+    private static PopUp popOnScreen;
+
+    public static void CreateOKButtonTitleTextPopUp(string title, string text, string buttonText, Action callback)
+    {
+        CreatePopUp();
+
+        popOnScreen.AddTitle(title);
+
+        popOnScreen.AddDescription(text);
+
+        popOnScreen.AddOneButton(buttonText, callback);
+    }
+
+    public static void CreateOKButtonTextPopUp(string text, string buttonText, Action callback)
+    {
+        CreatePopUp();
+
+        popOnScreen.AddDescription(text);
+
+        popOnScreen.AddOneButton(buttonText, callback);
+    }
+
+    public static void CreateDoubleButtonTitleTextPopUp(string title, string text, string firstButtonText, Action firstButtonCallback, string secondButtonText, Action secondButtonCallback)
+    {
+        CreatePopUp();
+
+        popOnScreen.AddTitle(title);
+
+        popOnScreen.AddDescription(text);
+
+        popOnScreen.AddTwoButtons(firstButtonText, firstButtonCallback, secondButtonText, secondButtonCallback);
+    }
+
+    public static void CreateDoubleButtonTextPopUp(string text, string firstButtonText, Action firstButtonCallback, string secondButtonText, Action secondButtonCallback)
+    {
+        CreatePopUp();
+
+        popOnScreen.AddDescription(text);
+
+        popOnScreen.AddTwoButtons(firstButtonText, firstButtonCallback, secondButtonText, secondButtonCallback);
+    }
+
+    private static void CreatePopUp()
+    {
+        Transform popUpContainer = GameObject.FindGameObjectWithTag("PopUpContainer").transform;
+
+        popOnScreen = UnityEngine.Object.Instantiate(Resources.Load<PopUp>("Prefabs/PopUpPrefab"), popUpContainer);
+    }
+}
