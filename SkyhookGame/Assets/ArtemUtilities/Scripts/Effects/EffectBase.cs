3bccc4c3ac8ff3a5e71e8a8cb2a595647eb7ab40
Artem <artem.stolyga@gmail.com>
2020-11-10 20:17:58 +0100

    Added an option to the editor to play the effect in reverse

--- SkyhookGame/Assets/ArtemUtilities/Scripts/Effects/EffectBase.cs
+++ SkyhookGame/Assets/ArtemUtilities/Scripts/Effects/EffectBase.cs
@@ -5,6 +5,7 @@ public abstract class EffectBase : MonoBehaviour
 {
     public bool autoReset = true;
     public bool playOnStart;
+    public bool playInReverse;
     //public bool useCurrentValueAsStart;
 
 
@@ -115,6 +116,17 @@ public abstract class EffectBase : MonoBehaviour
     public virtual void PlayEffect()
     {
         playEffect = true;
+
+        // If playInReverse is set to true in the editor
+        // we set the private reverse variable to true,
+        // and set the playTime to target time,
+        // since in reverse the playTime is being decreased
+        if (playInReverse)
+        {
+            reverse = true;
+
+            playTime = effectSO.tween.targetTime;
+        }
     }
 
     protected abstract void ApplyEffect();
