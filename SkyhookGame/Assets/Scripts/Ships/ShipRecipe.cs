e7795927389a0d5ba92c279e47ec550258e0306a
AArtlone <artem.stolyga@gmail.com>
2020-11-20 13:52:09 +0100

    Updated Manufactory Visuals with new recipes

--- SkyhookGame/Assets/Scripts/Ships/ShipRecipe.cs
+++ SkyhookGame/Assets/Scripts/Ships/ShipRecipe.cs
@@ -3,11 +3,15 @@
     public ShipsDSID shipID;
     public string shipName;
     public int price;
+    public int reqAluminium;
+    public int reqPlatinum;
 
-    public ShipRecipe(ShipsDSID shipID, string shipName, int price)
+    public ShipRecipe(ShipsDSID shipID, string shipName, int price, int reqAluminium, int reqPlatinum)
     {
         this.shipID = shipID;
         this.shipName = shipName;
         this.price = price;
+        this.reqAluminium = reqAluminium;
+        this.reqPlatinum = reqPlatinum;
     }
 }
--- SkyhookGame/Assets/Scripts/UI/Manufactory/BuildShipView.cs
+++ SkyhookGame/Assets/Scripts/UI/Manufactory/BuildShipView.cs
@@ -5,13 +5,15 @@ public class BuildShipView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI nameText = default;
     [SerializeField] private TextMeshProUGUI priceText = default;
-    [SerializeField] private TextMeshProUGUI reqResourcesText = default;
+    [SerializeField] private TextMeshProUGUI reqAluminiumText = default;
+    [SerializeField] private TextMeshProUGUI reqPlatinumText = default;
 
     public void ShowView(ShipRecipe shipRecipe)
     {
         nameText.text = shipRecipe.shipName;
         priceText.text = shipRecipe.price.ToString();
-        //reqResourcesText.text = shipRecipe.reqResources.ToString();
+        reqAluminiumText.text = shipRecipe.reqAluminium.ToString();
+        reqPlatinumText.text = shipRecipe.reqPlatinum.ToString();
 
         gameObject.SetActive(true);
     }
