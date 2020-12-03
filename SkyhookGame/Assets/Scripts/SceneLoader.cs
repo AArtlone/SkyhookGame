f748048d43a263a70ef1c05eb1b2682bfc1f7dcd
AArtlone <artem.stolyga@gmail.com>
2020-12-03 17:56:38 +0100

    Fixed a bug where all previews would open when one institution was clicked

---
+++ SkyhookGame/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using MyUtilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : Singleton<SceneLoader>
+{
+    List<MonoBehaviour> allWaiters = new List<MonoBehaviour>();
+
+    protected override void Awake()
+    {
+        SetInstance(this);
+    }
+
+    public WaitUntil WaitForLoading()
+    {
+        return new WaitUntil(() =>
+        {
+            print("waiting: " + allWaiters.Count);
+
+            if (allWaiters.Count == 0)
+                return true;
+            else
+                return false;
+        });
+    }
+
+    public void AddWaiter(MonoBehaviour mb)
+    {
+        if (!allWaiters.Contains(mb))
+            allWaiters.Add(mb);
+    }
+
+    public void RemoveWaiter(MonoBehaviour mb)
+    {
+        if (allWaiters.Contains(mb))
+            allWaiters.Remove(mb);
+    }
+}
--- SkyhookGame/Assets/Scripts/Settlement/InstitutionTouchController.cs
+++ SkyhookGame/Assets/Scripts/Settlement/InstitutionTouchController.cs
@@ -8,30 +8,70 @@ public class InstitutionTouchController : MonoBehaviour
 	/// when you tap/click on it to perform actions.
 	/// </summary>
 	[Header("The view (GUI) to display when pressing this institution")]
-	[SerializeField] private GameObject initial_view;
+	[SerializeField] private GameObject preview = default;
 
-	private void Update()
+    private void Awake()
+    {
+        if (preview == null)
+        {
+			Debug.LogWarning($"Initial_view is null on {gameObject.name}");
+			enabled = false;
+        }
+    }
+
+    private void Update()
 	{
-		if (initial_view.activeSelf) return;
+		if (preview.activeSelf) return;
+
+		if (Application.isEditor)
+		{
+			if (!Input.GetMouseButtonDown(0))
+				return;
+
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit2D[] hits = new RaycastHit2D[1];
+			var hitsCount = Physics2D.GetRayIntersectionNonAlloc(ray, hits, 100f);
+
+			if (hitsCount == 0)
+				return;
+
+			var hitGameObject = hits[0].collider.gameObject;
+
+			if (hitGameObject.layer != 9)
+				return;
+
+			if (hitGameObject != gameObject)
+				return;
+
+			preview.SetActive(true);
+			return;
+        }
+
 		if (Input.touchCount <= 0) return;
 
 		for (int i = 0; i < Input.touchCount; i++)
 		{
 			var touch = Input.GetTouch(i);
-			if (touch.phase == TouchPhase.Began)
-			{
-				var ray = Camera.main.ScreenPointToRay(touch.position);
-				var hits = Physics2D.RaycastAll(ray.origin, ray.direction);
 
-				foreach (var hit in hits)
-				{
-					// layer 9 is for an institution
-					if (hit.collider.gameObject.layer == 9)
-					{
-						initial_view.SetActive(true);
-					}
-				}
-			}
+			if (touch.phase != TouchPhase.Began)
+				continue;
+
+			var ray = Camera.main.ScreenPointToRay(touch.position);
+			RaycastHit2D[] hits = new RaycastHit2D[1];
+			var hitsCount = Physics2D.GetRayIntersectionNonAlloc(ray, hits, 100f);
+
+			if (hitsCount == 0)
+				return;
+
+			var hitGameObject = hits[0].collider.gameObject;
+
+			if (hitGameObject.layer != 9)
+				return;
+
+			if (hitGameObject != gameObject)
+				return;
+
+			preview.SetActive(true);
 		}
 	}
 }
